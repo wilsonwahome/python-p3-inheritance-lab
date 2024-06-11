b60206a532dff76039a78a2f55d440159cd76716
wahome <wahomewgitonga@gmail.com>
2024-06-11 19:22:54 +0300

    update

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,14 @@ from user import User
 
 import random
 
+from user import User
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]  
 
     def teach(self):
-        pass
+
+        import random
+        return random.choice(self.knowledge)
